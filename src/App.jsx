7d183bedda6740282ff95c60a957d2f83ec36563
Harshsheel-dev <23bca105@gsfcuniversity.ac.in>
2025-11-22 23:55:10 +0530

    fixed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import "./App.css";
 import CurrencyConvertor from "./components/currency-convertor";
 
+const url = `https://render-hosting-se2b.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
